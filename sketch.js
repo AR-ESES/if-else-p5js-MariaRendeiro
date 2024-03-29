e9030202acae2aed5d5c201a7e61e41775e7c7e7
MariaRendeiro <mariarcosta2003@gmail.com>
2024-03-29 23:42:25 +0000

    1

--- sketch.js
+++ sketch.js
@@ -1,11 +1,40 @@
-function setup() {
-  createCanvas(1280, 1024);
-  background(153);
-  stroke(0);
-  strokeWeight(2);
-  fill(237, 28, 36);
-  circle(30, 30, 40);
-  line(30, 30, 1280 / 2, 1024 / 2);
+function setup(){
+  createCanvas(400,400);
+  background(255);
 }
 
-function draw() {}
+function draw() {
+  background(255);
+  stroke(0);
+  strokeWeight(3);
+  
+  if(mouseX<width/2) {
+    if(mouseY<height/2){
+      fill(255,0,0);
+    
+    }
+    else{
+      fill(0,255,0);
+    }
+  }
+  
+  
+  if (mouseX > width / 2) 
+   if (mouseY < height / 2) {
+    fill(0, 255, 0); 
+  }
+  
+  if (mouseX > width / 2) 
+   if (mouseY > height / 2) {
+    fill(0, 0, 0); 
+  }
+  
+ if (mouseX < width / 2) {
+  if (mouseY > height / 2) 
+    fill(255, 255, 0); 
+  }
+      
+   
+  circle(width/2,height/2,height/4);
+  
+}
